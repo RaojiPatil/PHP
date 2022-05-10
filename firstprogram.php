a57969be102cb4975afd6e40cd834f795f06e86f
Raoji Patil <raojipatil22@gmail.com>
2022-05-10 13:50:15 +0530

    update

--- firstprogram.php
+++ firstprogram.php
@@ -8,13 +8,37 @@
 </head>
 <body>
 
-  <?php 
+<?php 
   // print hello world 
   $var = "Hello World!";
   echo $var;
+?>
   
-  ?>
+
+
+<?php
+$x = 5; // who work global scope
+
+function scope() {
+    //we can not use x variable inside this function becouse global scope;
+
+  echo "<p>Variable x inside function is: $x</p>";
+} 
+scope();
+// here easily we use x variable
+echo "<p>Variable x outside function is: $x</p>";
+?>
+
+
+
+
+
 
 </body>
 </html>
+
+
+
+
+
 
